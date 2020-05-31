5d264237cd210492e75b2f833d7a9765b9335dab
Matheus Robert <roberttjmelo@hotmail.com>
2020-05-31 12:56:42 -0300

    Change in Partners

--- src/pages/Partners/index.js
+++ src/pages/Partners/index.js
@@ -1,75 +1,81 @@
-import React, { useState } from 'react'
-import { View, Text, Image, FlatList, TouchableOpacity, Alert } from 'react-native'
-import styles from './styles'
+import React, { useState, useEffect } from 'react'
+import { View, Text, FlatList, TouchableOpacity, Alert } from 'react-native'
+
+import styles,{
+  Container,
+  LabelPage,
+  Body,
+  Partner,
+  Datas,
+  Button,
+  ButtonLabel,
+  NameCurrent,
+  Image,
+  Footer,
+  Header,
+  ImageCurrent
+} from './styles'
+
+
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 import userImg from '../../assets/images/userImg.png'
 
-const DATA = [
-  {
-  id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-  name: 'First part',
-  },
-  {
-  id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-  name: 'Second part',
-  },
-  {
-  id: '58694a0f-3da1-471f-bd96-145571e29d72',
-  name: 'Third part',
-  },
-  
-];
-function select(id){
-  Alert.alert(id)
-}
-function Item({ id, name, selected }) {
-  return (
-    <View
-      style={styles.item}
-    > 
-      <View style={styles.dados}>
-        <Image style={styles.imageCard} source={userImg} width={50} height={50} />
-        <Text style={styles.nomeAtual}>{name}</Text>
-      </View>
-      <TouchableOpacity onPress={() => {}} style={styles.button}>
-          <Icon style={styles.icon} name="toggle-up" size={20} color="#FFFFFF" />
-          <Text style={styles.buttonText}>Tornar atual</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
+
+
+import api, { showError, showSuccess } from '../../services/api'
 
 export default function Parceiros(){
     const [ atual, setAtual ] = useState('Fulano X')  
-  
+    const [ partners, setPartners] = useState([])
 
+
+    async function getPartners(){
+      //await setLoading(true)
+      try {
+          const res = await api.get('partners')
+          await setPartners(res.data)  
+          //setLoading(false)        
+      }catch(err){  
+        //showError('Parceiros não encontrados')
+        await setPartners(null)
+          //setLoading(false)  
+      }
+    
+    } 
+
+    useEffect(
+        () => {     
+            getPartners()
+        }
+    );
     return (
-        <View style={styles.container}>
-            <Text style={styles.titlePage}>Parceiro atual</Text>
-            <View style={styles.header}> 
-                <Image style={styles.image} source={userImg} width={50} height={50} />
-                <Text style={[styles.nomeAtual,{color:'white'}]}>{atual}</Text>
-            </View>
-            <View style={styles.body}>
-            <FlatList
-              data={DATA}
-              renderItem={({ item }) => (
-                <Item
-                  id={item.id}
-                  name={item.name}
-                  
-                  
-                />
-              )}
-              keyExtractor={item => item.id}
-              
-             
-            />
-            </View>
-            <View style={styles.footer}>
+        <Container>
+            <LabelPage>Parceiro atual</LabelPage>
+            <Header> 
+                <ImageCurrent source={userImg} />
+                <NameCurrent style={{color:'white'}}>{atual}</NameCurrent>
+            </Header>
+            <Body>
+                { partners 
+                ? partners.map((partner) => (
+                    <Partner key={partner.id}>
+                        <Datas>
+                          <Image source={userImg} />
+                          <NameCurrent>{partner.name}</NameCurrent>
+                        </Datas>
+                        <Button onPress={() => {}}>
+                          <Icon style={styles.icon} name="toggle-up" size={20} color="#FFFFFF" />
+                          <ButtonLabel>Tornar Atual</ButtonLabel>
+                        </Button>
+                    </Partner>
+                  ))
+                :<ButtonLabel>Você não possuí parceiros</ButtonLabel>
+                }
+            </Body>
+            <Footer>
 
-            </View>
-        </View>
+            </Footer>
+        </Container>
     )
 }
--- src/pages/Partners/styles.js
+++ src/pages/Partners/styles.js
@@ -1,5 +1,79 @@
 import { StyleSheet } from 'react-native'
 import varStyles from '../../varStyles'
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+    flex: 1;
+    background: ${varStyles.colors.contrasteDark};
+`
+export const LabelPage = styled.Text`
+    color: ${varStyles.colors.text};
+    font-size: 24px;
+    background: ${varStyles.colors.contraste};
+    text-align: center;
+`
+export const Body = styled.ScrollView.attrs(()=> ({
+    showsHorizontalScrollIndicator: false,
+    contentContainerStyle: {
+        alignItems: 'center',
+        paddingLeft: 16
+    }
+}))`
+    flex:6;
+    background: ${varStyles.colors.contrasteDark};
+`   
+
+export const Partner = styled.View`
+    background:#FFFFFF;
+    padding: 20px;
+    margin: 8px 16px;
+    border-radius:18px;
+    justify-content: center;
+    align-items: center;
+`
+export const Datas = styled.View`
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`
+export const Button = styled.TouchableOpacity`
+    width: 60%;
+    height:50px;
+    margin: 2%;
+    background: ${varStyles.colors.contraste};
+    border-radius: 15px;
+    flex-direction: row;
+    justify-content:space-evenly;
+    align-items: center; 
+`
+export const Image = styled.Image`
+    width: 50px;
+    height: 50px;
+`
+export const NameCurrent = styled.Text`
+    font-size: 18px;
+    color: black;
+`
+export const ButtonLabel = styled.Text`
+    color: ${varStyles.colors.text};
+`
+export const Footer = styled.View`
+
+`
+export const Header = styled.View`
+    flex:1;
+    background: ${varStyles.colors.contraste};
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+    border-bottom-right-radius:18px;
+    border-bottom-left-radius:18px;
+    padding: 0 30%;
+`
+export const ImageCurrent = styled.Image`
+    width: 50%;
+    height: 100%;
+`
 
 export default StyleSheet.create({
     container: {
@@ -20,9 +94,7 @@ export default StyleSheet.create({
         height: 50, 
     },
     dados:{
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center'
+       
     },
     title: {
         fontSize: 32,
